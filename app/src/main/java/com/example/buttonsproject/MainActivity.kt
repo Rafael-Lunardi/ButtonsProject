952dc781aeb4686459fd07f91dfaef397fb0a313
RafaelLunardiDeCast <rafaell53@nycstudents.net>
2025-10-15 14:47:27 -0400

    initial commit

---
+++ app/src/main/java/com/example/buttonsproject/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.buttonsproject
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.buttonsproject.ui.theme.ButtonsProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Log.i("MainActivity", "SetContent")
+            MainLayout()
+        }
+    }
+}
+
+
+@Composable
+fun MainLayout() {
+    Surface(
+        color = Color.Cyan,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "Android",
+                fontSize = 36.sp
+            )
+        }
+    }
+
+}
+
+
+@Preview
+@Composable
+fun MainLayoutPreview() {
+    MainLayout()
+}
